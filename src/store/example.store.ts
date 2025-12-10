0b66bbe9f587a608450e78e93914b13efd1b131c
nitesh-dev <niteshdev547@gmail.com>
2025-12-10 20:42:03 +0530

    fix

--- src/store/example.store.ts
+++ src/store/example.store.ts
@@ -2,7 +2,6 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-import { CheckField } from "./types";
 import { debouncedChromeStorage } from "./storage/debouncedStorage";
 
 
